ec3d562b2d7b28b19a2e66a6636fe2b1d8387b43
Tomas <tomas@debian.com>
2025-07-16 18:01:48 -0300

    add: Property dto

--- src/dto/PropertyDTO.ts
+++ src/dto/PropertyDTO.ts
@@ -1,3 +1,14 @@
+import { AddressDTO } from "./AddressDTO";
+import { ContactInfoDTO } from "./ContactInfoDTO";
+import { PoliciesDTO } from "./PoliciesDTO";
+
 export interface CreatePropertyDTO {
+  id: number;
   propertyName: string;
+  description: string;
+  address: AddressDTO;
+  contactInfo: ContactInfoDTO;
+  policies: PoliciesDTO;
+  createdAt: Date;
+  updatedAt: Date;
 }
