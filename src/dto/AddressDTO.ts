963c0657f9ae5270c47e3dbe40dd294ba35ea61b
Tomas <tomas@debian.com>
2025-07-16 13:09:48 -0300

    add: address dto

---
+++ src/dto/AddressDTO.ts
@@ -0,0 +1,12 @@
+export interface AddressDTO {
+  houseNumber: string;
+  street: string;
+  city: string;
+  postalCode: string;
+  state: string;
+  country: string;
+  countryCode: string;
+  lat: number;
+  lon: number;
+  osmId: string;
+}
