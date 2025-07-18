15b49e87e2b2bf891de5c51bf3fbc45618b86018
Tomas <tomas@debian.com>
2025-07-18 11:38:22 -0300

    add: se agregaron metodos de repositorio a la interfaz

--- src/domain/ports/IUserRepository.ts
+++ src/domain/ports/IUserRepository.ts
@@ -5,4 +5,8 @@ export interface IUserRepository {
   save(user: User): Promise<void>;
   // Establece el contrato de la funcion que deberia buscar un usuario por username
   findByUsername(username: string): Promise<User>;
+  // Funcion que deberia buscar un usuario por id
+  findById(id: string): Promise<User>;
+  // Funcion que deberia actualizar el mail de usuario como valido
+  validateEmail(id: number): Promise<void>;
 }
