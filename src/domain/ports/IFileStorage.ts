a0f557a057699c880459e67aada793d180fbaa41
Tomas <tomas@debian.com>
2025-07-21 17:22:18 -0300

    add: se agrego metodo delete en IFileStorage

--- src/domain/ports/IFileStorage.ts
+++ src/domain/ports/IFileStorage.ts
@@ -1,7 +1,11 @@
 export interface IFileStorage {
+  // Guardar imagene en disco
   save(
     buffer: Buffer,
     filename: string,
     destinationFolder: string,
   ): Promise<{ publicUrl: string; relativeUrl: string }>;
+
+  // Eliminar imagen en disco.
+  delete(path: string): Promise<void>;
 }
