59ecf216cab2b94bca82e159b4b796ab543d3053
Tomas <tomas@debian.com>
2025-07-21 17:22:51 -0300

    add: se agrego metodo delete en IPropertyImageRepo

--- src/domain/ports/IPropertyImageRepository.ts
+++ src/domain/ports/IPropertyImageRepository.ts
@@ -1,7 +1,15 @@
 import { PropertyImage } from "../entities/PropertyImage";
 
 export interface IPropertyImageRepository {
+  // Buscar todas las imagenes de la propiedad
   findAllPropertyImages(propertyId: number): Promise<Array<PropertyImage>>;
 
+  // Buscar una imagen de la propiedad
+  findById(propertyId: number, imageId: number): Promise<PropertyImage>;
+
+  // Guardar imagenes en la base de datos
   save(images: Array<PropertyImage>): Promise<void>;
+
+  // Eliminar imagenes
+  delete(imageId: number): Promise<void>;
 }
