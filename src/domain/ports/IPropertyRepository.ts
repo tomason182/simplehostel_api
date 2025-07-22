2a8c3b525825188263c7298b02a267635475c88b
Tomas <tomas@debian.com>
2025-07-22 12:03:24 -0300

    add: Interfaz para propertyRepository

--- src/domain/ports/IPropertyRepository.ts
+++ src/domain/ports/IPropertyRepository.ts
@@ -2,4 +2,6 @@ import { Property } from "../entities/Property";
 
 export interface IPropertyRepository {
   save(property: Property): Promise<void>;
+
+  findPropertyDetails(propertyId: number): Promise<Property>;
 }
