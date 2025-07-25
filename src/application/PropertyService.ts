173121fa4687d6699af6306de50362a04d1cc46c
Tomas <tomas@debian.com>
2025-07-25 11:53:18 -0300

    add: Se agregaron metodos para establecer descripcion y politicas

--- src/application/PropertyService.ts
+++ src/application/PropertyService.ts
@@ -9,10 +9,16 @@ import { Policies } from "../domain/value-objects/Policies";
 import { IPropertyRepository } from "../domain/ports/IPropertyRepository";
 import { IFacilityRepository } from "../domain/ports/IFacilityRepository";
 import { IPoliciesRepository } from "../domain/ports/IPoliciesRepository";
-import { AddressDTO } from "../dto/AddressDTO";
 import { IAddressRepository } from "../domain/ports/IAddressRepository";
-import { ContactInfoDTO } from "../dto/ContactInfoDTO";
 import { IContactInfoRepository } from "../domain/ports/IContactInfoRepository";
+
+import { AddressDTO } from "../dto/AddressDTO";
+import { ContactInfoDTO } from "../dto/ContactInfoDTO";
+import {
+  GeneralPoliciesDTO,
+  MinorPoliciesDTO,
+  OtherPoliciesDTO,
+} from "../dto/PoliciesDTO";
 
 export class PropertyService {
   constructor(
@@ -107,4 +113,143 @@ export class PropertyService {
       msg: "PROPERTY_UPDATED",
     };
   }
+
+  async updatePropertyFacilities(
+    propertyId: number,
+    submittedFacilitiesIds: Array<number>,
+  ): Promise<{ status: string; msg: string }> {
+    // Iniciar un transaccción. PropertyService.prototype.updatePropertyFacilities.useTransacction = true;
+
+    // 1. Validar que la propiedad existe.
+    const property = await this.propertyRepository.findById(propertyId);
+    if (property === null) {
+      return { status: "error", msg: "PROPERTY_NOT_FOUND" };
+    }
+
+    // 2. Validar que todas las facilities sean validas.
+    if (submittedFacilitiesIds.length > 0) {
+      const allExist = await this.facilityRepository.doAllExist(
+        submittedFacilitiesIds,
+      );
+
+      if (allExist.length === 0) {
+        return {
+          status: "error",
+          msg: "INVALID_FACILITIES_IDS",
+        };
+      }
+    }
+
+    // 3. Obtener el listado de facilities actuales de la propiedad.
+    const currentFacilitiesIds =
+      await this.facilityRepository.getFacilitiesIdsForProperty(propertyId);
+
+    // 4. Calcular diferencias.
+    const idsToAdd = submittedFacilitiesIds.filter(
+      (id) => !currentFacilitiesIds.includes(id),
+    );
+    const idsToRemove = currentFacilitiesIds.filter(
+      (id) => !submittedFacilitiesIds.includes(id),
+    );
+
+    // 5. Aplicar cambios
+    if (idsToAdd.length > 0) {
+      await this.facilityRepository.addFacilitiesToProperty(
+        propertyId,
+        idsToAdd,
+      );
+    }
+
+    if (idsToRemove.length > 0) {
+      await this.facilityRepository.removeFacilitiesFromProperty(
+        propertyId,
+        idsToRemove,
+      );
+    }
+
+    return {
+      status: "ok",
+      msg: "FACILITIES_UPDATED",
+    };
+  }
+
+  async updateDescription(
+    propertyId: number,
+    description: string,
+  ): Promise<{ status: string; msg: string }> {
+    const property = await this.propertyRepository.findById(propertyId);
+
+    if (property === null) {
+      return {
+        status: "error",
+        msg: "PROPERTY_NOT_FOUND",
+      };
+    }
+
+    property.setDescription(description);
+
+    await this.propertyRepository.updateDescription(property);
+
+    return {
+      status: "ok",
+      msg: "PROPERTY_UPDATED",
+    };
+  }
+
+  async updateGeneralPolicies(
+    propertyId: number,
+    generalPoliciesDTO: GeneralPoliciesDTO,
+  ): Promise<{ status: string; msg: string }> {
+    const generalPolicies = new GeneralPolicies(
+      generalPoliciesDTO.minLengthStay,
+      generalPoliciesDTO.maxLengthStay,
+      generalPoliciesDTO.minAdvanceBooking,
+      generalPoliciesDTO.breakfastIncluded,
+      generalPoliciesDTO.checkInFrom,
+      generalPoliciesDTO.checkInUntil,
+      generalPoliciesDTO.checkOutFrom,
+      generalPoliciesDTO.checkOutUntil,
+    );
+
+    await this.policiesRepository.saveGeneralPolicies(
+      propertyId,
+      generalPolicies,
+    );
+
+    return {
+      status: "ok",
+      msg: "POLICIES_UPDATED",
+    };
+  }
+
+  async updateMinorPolicies(
+    propertyId: number,
+    minorPoliciesDTO: MinorPoliciesDTO,
+  ): Promise<{ status: string; msg: string }> {
+    const minorPolicies = MinorPolicies.fromDTO(minorPoliciesDTO);
+
+    await this.policiesRepository.saveMinorPolicies(propertyId, minorPolicies);
+
+    return {
+      status: "ok",
+      msg: "POLICIES_UPDATED",
+    };
+  }
+
+  async updateOtherPolicies(
+    propertyId: number,
+    otherPoliciesDTO: OtherPoliciesDTO,
+  ): Promise<{ status: string; msg: string }> {
+    const otherPolicies = OtherPolicies.fromDTO(otherPoliciesDTO);
+
+    await this.policiesRepository.saveOtherPolicies(propertyId, otherPolicies);
+
+    return {
+      status: "ok",
+      msg: "POLICIES_UPDATED",
+    };
+  }
 }
+
+(PropertyService.prototype.updatePropertyFacilities as any).useTransaction =
+  true;
